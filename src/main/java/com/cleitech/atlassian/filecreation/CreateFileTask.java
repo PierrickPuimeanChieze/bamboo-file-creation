65ca3dc89419ef59fc599acb6a87588bc92acfda
Pierrick Puimean-Chieze <pierrick.puimean-chieze@formatech.be>
2015-04-05 11:37:19 +0200

    Corrected the internationalization of error message

--- src/main/java/com/cleitech/atlassian/filecreation/CreateFileTask.java
+++ src/main/java/com/cleitech/atlassian/filecreation/CreateFileTask.java
@@ -31,29 +31,29 @@ public class CreateFileTask implements TaskType {
         //We retrieve the file content from the configuration
         final String fileContent = taskContext.getConfigurationMap().get(CreateFileTaskConfigurator.CONTENT_CONFIG_KEY);
 
-        buildLogger.addBuildLogEntry("File Content"+fileContent);
+        buildLogger.addBuildLogEntry("File Content :"+fileContent);
         //Same for the path
         final String filePath = taskContext.getConfigurationMap().get(CreateFileTaskConfigurator.FILEPATH_CONFIG_KEY);
         File destinationFile = new File(taskContext.getWorkingDirectory(), filePath);
         File parentDirectory = destinationFile.getParentFile();
         I18nBean i18nBean = i18nBeanFactory.getI18nBean();
         if (destinationFile.exists()) {
-            throw new TaskException(i18nBean.getText("filecreation.fileExists"));
+            throw new TaskException(i18nBean.getText("filecreation.fileExists.error", new Object[]{destinationFile.getAbsolutePath()}));
         }
         //If the parent directory doesn't exists we try to created it
         if (!parentDirectory.exists()) {
             //If it is impossible, we throw an Exception
             if (!parentDirectory.mkdirs()) {
-                throw new TaskException(i18nBean.getText("filecreation.parent.unableToCreate"));
+                throw new TaskException(i18nBean.getText("filecreation.parent.unableToCreate.error", new Object[]{parentDirectory.getAbsolutePath()}));
             }
         //But if it is exists, but is in fact NOT a dir, we also throw an exception
         } else if (!parentDirectory.isDirectory()) {
-            throw new TaskException(i18nBean.getText("filecreation.parent.notADirectory"));
+            throw new TaskException(i18nBean.getText("filecreation.parent.notADirectory.error", new Object[]{parentDirectory.getAbsolutePath()}));
         }
         try (PrintWriter printWriter = new PrintWriter(destinationFile)){
             printWriter.write(fileContent);
         } catch (FileNotFoundException e) {
-            throw new TaskException(i18nBean.getText("filecreation.unableToWrite"));
+            throw new TaskException(i18nBean.getText("filecreation.unableToWrite.error", new Object[]{destinationFile.getAbsolutePath()}));
         }
 
         buildLogger.addBuildLogEntry(destinationFile.getAbsolutePath());
